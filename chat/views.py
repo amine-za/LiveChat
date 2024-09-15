f9d18dff2f4bf56ee7940e54f4f5135ea41e6319
amine zaghloul <115177542+amine-za@users.noreply.github.com>
2024-09-15 02:00:56 +0000

    fixed room problem

--- chat/views.py
+++ chat/views.py
@@ -16,11 +16,14 @@ def HomeView(request):
         
 
 def RoomView(request, room_name, username):
-    # existing_room = Room.objects.get(room_name__icontain=room_name)
-    get_messages = Message.objects.filter(room=room_name)
+    try:
+        RoomModel = Room.objects.get(room_name=room_name)
+    except Room.DoesNotExist:
+        return (HttpResponse("Room not found", status=404))
+    get_messages = Message.objects.filter(room=RoomModel)
     context = {
-        "messages": "test message",
+        "messages": get_messages,
         "user": username,
-        "room_name": room_name
+        "room_name": RoomModel
     }
     return render(request, "room.html", context)
